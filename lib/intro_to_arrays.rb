c619d5cd0c15d08e518a8178b97c46bb200dc459
Nicholas  Luong <nckluong@gmail.com>
2018-08-13 18:45:12 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,20 @@
+def instantiate_new_array
+  arr = [] 
+end 
+
+def array_with_two_elements
+  arr = [1,2]
+end 
+
+def first_element(array)
+  array[0]
+end 
+
+def third_element(array)
+  array[2]
+end 
+
+def last_element(array)
+  array[-1]
+end 
+
